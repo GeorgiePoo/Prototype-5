e46bafb5505714f9e9563f4e574ed3e3824cba3b
GeorgiePoo <121971574+GeorgiePoo@users.noreply.github.com>
2023-04-05 11:01:46 -0700

    Started 5.3

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -55,6 +55,7 @@ public class Target : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad")) { gameManager.GameOver(); }
     }
 }
 
